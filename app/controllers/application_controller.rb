a90a21333e0e7db76152ada478332d092af69176
Corinn Pope <corinn.pope@gmail.com>
2020-05-24 12:35:00 -0500

    fix(acts_as_tenant): remove subdomain configuration in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :masquerade_user!
 
-  set_current_tenant_by_subdomain(:user, :subdomain)
+    set_current_tenant_through_filter
+    before_action :find_current_tenant
   
+    def find_current_tenant
+      # set to Account.first for now, you'll change this later
+      set_current_tenant(current_user)
+    end
+
   protected
 
 
